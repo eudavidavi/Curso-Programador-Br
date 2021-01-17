b8bf9f76c3025b8fb5d608a7b37f49c8fedf178f
davidavi <54958760+monteirodavi@users.noreply.github.com>
2021-01-17 19:51:41 -0300

    Primeiras aulas JS

---
+++ 08. Javascript Básico/05. Tipos de Dados/script.js
@@ -0,0 +1,18 @@
+// Boolean: permite fazer operaçoes logicas
+//null
+//Undefined
+//Number
+//String: cadeia de caracteres. Concatenar = juntar strings
+
+/*
+Comentário em mais de uma linha.
+*/
+
+
+var boleano = true;
+var nulo = null;
+var indefinido;
+var numero = 12;
+var cadeia = "string \n string em outra linha";
+
+console.log(boleano, nulo, indefinido, numero, cadeia)
---
+++ 08. Javascript Básico/06. Operações Aritiméticas/script.js
@@ -0,0 +1,31 @@
+/*
+
++ Adição
+- Subtração
+* Muliplicação
+/ Divisão
+% Módulo
+++ Incremento: Diferença entre "++"" antes e depois.
+-- Decremento
+
+*/
+
+var num1 = 7
+var num2 = 4
+var resSoma = num1 + num2
+var resSub = num1 - num2
+var resMult = num1 * num2
+var resDiv = num1 / num2
+var resMod = num1 % num2
+var resInc1 = ++num1
+var resDecr1 = --num1
+
+console.log(num1);
+console.log(num2);
+console.log(resSoma);
+console.log(resSub);
+console.log(resMult);
+console.log(resDiv);
+console.log(resMod);
+console.log(resInc1);
+console.log(resDecr1);
