9992a27533c195c6a6e52fa30d807a7577efbd11
davidavi <54958760+monteirodavi@users.noreply.github.com>
2021-01-17 22:16:41 -0300

    Exercicios 9 & 10

--- 08. Javascript Básico/09. Operadores Lógicos/script.js
+++ 08. Javascript Básico/09. Operadores Lógicos/script.js
@@ -1,3 +1,12 @@
 /*
 &&, ||, ~
 */
+
+var idade = 18
+var menos18 = idade < 18
+var mais16 = idade > 16
+
+console.log("Maior que 18 ", menos18);
+console.log("Menor que 16 ", mais16);
+
+console.log("Entre 16 e 18", mais16 && menos18);
