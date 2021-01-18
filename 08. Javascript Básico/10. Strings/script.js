d236701bd64304dd50f9d0c9de14b79ac0d5eec8
davidavi <54958760+monteirodavi@users.noreply.github.com>
2021-01-18 08:17:21 -0300

    Exercícios 9 & 10

---
+++ 08. Javascript Básico/10. Strings/script.js
@@ -0,0 +1,16 @@
+// String com aspas duplas ou aspas simples.
+
+var str = 'Um texto "com aspas".';
+
+console.log(str);
+
+str += " Mais outro texto. "; 
+
+console.log(str);
+
+str+= 9
+
+console.log("Concatenado com número: ", str);
+
+console.log(9);
+
