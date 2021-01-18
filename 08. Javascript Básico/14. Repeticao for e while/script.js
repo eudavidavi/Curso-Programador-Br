5f8f3863c0d334c0e5fad6b8931bbebab2c3b846
davidavi <54958760+monteirodavi@users.noreply.github.com>
2021-01-18 18:59:24 -0300

    Exercicio For/While

---
+++ 08. Javascript Básico/14. Repeticao for e while/script.js
@@ -0,0 +1,17 @@
+//FOR
+
+var numero = 10;
+
+for (var vez = 1; vez <= numero; vez++){
+    console.log("Essa foi a repetição numero " + vez + "." )
+    }
+
+console.log("Acabaram-se as repetições.")
+
+//WHILE
+while (numero > 0) {
+    console.log("Contagem regressiva " + numero + "... ")
+    numero--
+};
+
+console.log("Chegou a hora! Olá Mundo!")
