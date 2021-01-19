61c0ac0de1e09913854214bbc5ff0f3252852b30
davidavi <54958760+monteirodavi@users.noreply.github.com>
2021-01-19 19:59:54 -0300

    Update script.js

--- 08. Javascript Básico/08. Comparativos/script.js
+++ 08. Javascript Básico/08. Comparativos/script.js
@@ -7,11 +7,11 @@ Comparativos:
 >, <, >=, <=
 */
 
-var num1 = 2
-var num2 = 2
-var fakeNum = "2"
+var num1 = 2;
+var num2 = 2;
+var fakeNum = "2";
 
-console.log(num1 == num2)
-console.log(num1 === num2)
-console.log(num1 == fakeNum)
-console.log(num1 === fakeNum)
+console.log(num1 == num2);
+console.log(num1 === num2);
+console.log(num1 == fakeNum);
+console.log(num1 === fakeNum);
