bb5989b924f7e2db9a52ec07a0bb7da71201ecf4
davidavi <54958760+monteirodavi@users.noreply.github.com>
2021-01-20 11:04:29 -0300

    Arrays e Funções

---
+++ 08. Javascript Básico/15. Arrays/script.js
@@ -0,0 +1,32 @@
+//Array: RECEBE QUAISQUER TIPOS DE DADOS
+//Forma de criar array vazio
+var vetor = new Array();
+
+//Preenchendo o vetor (pode criar já "preenchido")
+vetor = ["Davi", "Julia", "Pedro", "Carmem"];
+
+//Imprime a partir do índice zero
+console.log(vetor[0]);
+
+//Tamanho do Array
+console.log(vetor.length);
+
+//Imprimir o array completo
+console.log(vetor);
+
+//Imprimir os indices separados do array 
+for (i = 0; i < vetor.length; i++){
+    console.log(vetor[i])
+};
+
+//Forma resumida de fazer o for de cima: "in" pega o índice, "of" pega o elemento 
+
+//in
+for (var i in vetor){
+    console.log(i)
+};
+
+//of
+for (var i of vetor){
+    console.log(i)
+};
