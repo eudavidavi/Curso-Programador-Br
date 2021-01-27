fb4ea25a9887605a8b7f3ee5b54813dbed011141
davidavi <54958760+monteirodavi@users.noreply.github.com>
2021-01-27 20:10:26 -0300

    Construtores 2

--- 08. Javascript Básico/20. Objetos - Construtores/script.js
+++ 08. Javascript Básico/20. Objetos - Construtores/script.js
@@ -1,1 +1,16 @@
 //Construtor: criar objeto a partir de uma função
+
+function Pessoa (nome, sobrenome, idade) {
+this.nome = nome;
+this.sobrenome = sobrenome;
+this.idade = idade
+
+this.nomeCompleto = function () {
+    return "Seu nome é " + this.nome + " " + this.sobrenome + ". Sua idade é " +this.idade + ".";
+    }
+}
+
+var testePessoa = new Pessoa("Davi", "Monteiro", 29)
+
+console.log(testePessoa.nomeCompleto());
+
