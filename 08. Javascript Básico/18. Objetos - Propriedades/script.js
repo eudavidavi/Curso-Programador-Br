d592b7c2618cc8170f0454259d0ed1e3abc4b048
davidavi <54958760+monteirodavi@users.noreply.github.com>
2021-01-27 09:01:27 -0300

    Propriedades

---
+++ 08. Javascript Básico/18. Objetos - Propriedades/script.js
@@ -0,0 +1,25 @@
+// Array [] || Objeto {}
+var objeto = {}
+
+//Objeto: {chave:"valor"}
+var objeto = {nome:"Bola", forma:"esfera", donos: ["João", "Maria", "Davi"]}
+
+//Elemento > Variável || Objeto > Propriedade
+
+//Acessar as Propriedades do Objeto 1 > objeto.propriedade
+console.log(objeto.nome);
+
+console.log(objeto.donos[2]);
+//Acessar as Propriedades do Objeto 2 > objeto["propriedade"]
+console.log(objeto["forma"]);
+
+//Adicionar propriedades
+objeto.fura = false;
+objeto["tamanho"] = 20;
+console.log(objeto);
+
+//Adicionar uma variável dentro do Objeto
+var teste = "umTeste";
+objeto[teste] = "oTeste";
+
+console.log(objeto);
