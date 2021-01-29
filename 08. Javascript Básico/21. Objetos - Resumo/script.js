71008599e08a742c43134f5150fe89993dad3a37
davidavi <54958760+monteirodavi@users.noreply.github.com>
2021-01-29 08:15:57 -0300

    Objeto - Resumo

--- 08. Javascript Básico/21. Objetos - Resumo/script.js
+++ 08. Javascript Básico/21. Objetos - Resumo/script.js
@@ -1,10 +1,24 @@
 //Objeto e array vazios.
-var objeto = {}
+var obj = {}
 var arr = []
 
 // três formas de criar objetos
-//1.
 
-//2.
+//1. Cria já com os valores dentro
+var objeto = {nome:"valor", outroNome: "outro valor"}
+
+//2. Atraves de uma função
+function nomeCompleto(name, lastName) {
+    return { nome: name, sobrenome: lastName}
+}
 
 //3. Protótipo
+function nomeCompleto2(name, lastName) {
+    this.nome = name;
+    this.sobrenome = lastName;
+}
+
+//Para imprimir é preciso passar os valores
+var nomeImpresso = new nomeCompleto2 ("nome", "sobrenome");
+
+console.log(nomeImpresso);
