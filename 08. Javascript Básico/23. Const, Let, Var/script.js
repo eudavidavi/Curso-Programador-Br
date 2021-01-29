a86c575b56d3f9b5473763175054aa19b4329e1f
davidavi <54958760+monteirodavi@users.noreply.github.com>
2021-01-29 08:50:36 -0300

    Const Let Var

--- 08. Javascript Básico/23. Const, Let, Var/script.js
+++ 08. Javascript Básico/23. Const, Let, Var/script.js
@@ -1,3 +1,21 @@
 //Var:
-//Const (ES6):
-//Let (ES6):
+//Const (ES6): Constante > uma vez atribuído, não pode ser REATRIBUIDO.
+const constante = [4];
+console.log(constante);
+
+constante.push(5);
+console.log(constante);
+
+//Let (ES6): diferença pro "var" é de escopo. Let e Const tem escopo LOCAL.
+var variavel = "var"
+let lete = "let"
+
+console.log(variavel);
+console.log(lete);
+console.log(constante);
+
+{console.log(variavel);
+    console.log(lete);
+    console.log(constante);
+  
+}
